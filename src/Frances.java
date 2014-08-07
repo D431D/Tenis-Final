7715961a6ca455d3cb0067a5143d427006311ec9
wolv16 <wolv16@hotmail.com>
2014-08-07 14:26:20 -0500

    Create Frances.java

---
+++ src/Frances.java
@@ -0,0 +1,32 @@
+public class Frances implements Idioma {
+    
+    public String getLove(){
+        return "love";
+    }
+    public String get0(){
+        return "Zéro";
+    }
+    public String get15(){
+        return "Quize ";
+    }
+    public String get30(){
+        return "Trente ";
+    }
+    public String get40(){
+        return "cuarenta";
+    }
+    public String getDeuce(){
+        return "Deuce";
+    }
+    public String getAll(){
+        return "Égal";
+    }
+    public String getAdvantage(){
+        return "Adventage ";
+    }
+    
+    public String getWin(){
+        return " Victories";
+    }
+    
+}
