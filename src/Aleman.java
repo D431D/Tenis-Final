3943b455f6996fbc7c944b9e0e4ba19269243ed7
wolv16 <wolv16@hotmail.com>
2014-08-07 14:19:38 -0500

    Create Aleman.java
    puntuacion en idioma aleman

---
+++ src/Aleman.java
@@ -0,0 +1,34 @@
+public class Aleman implements Idioma{
+
+    public String getLove(){
+        return "Liebe";
+    }    
+    public String get0(){
+        return "Liebe";
+    }
+    public String get15(){
+        return "Funfzehn";
+    }
+    public String get30(){
+        return "Dreibig";
+    }
+    public String get40(){
+        return "Vierzig";
+    }
+    public String getDeuce(){
+        return "Ziehen";
+    }
+    public String getAll(){
+        return "Alle";
+    }
+    public String getAdvantage(){
+        return "Bewegt ";
+    }
+    
+    public String getWin(){
+        return " Gewinn";
+    }
+    
+
+    
+}
