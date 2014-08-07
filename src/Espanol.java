8cc22baaacde5eeace449bff53aba531fb065b13
wolv16 <wolv16@hotmail.com>
2014-08-07 14:20:46 -0500

    Create Espanol.java
    puntuacion en español

---
+++ src/Espanol.java
@@ -0,0 +1,32 @@
+public class Espanol implements Idioma {
+    
+    public String getLove(){
+        return "Cero";
+    }
+    public String get0(){
+        return "Cero ";
+    }
+    public String get15(){
+        return "Quince ";
+    }
+    public String get30(){
+        return "Treinta ";
+    }
+    public String get40(){
+        return "Cuarenta";
+    }
+    public String getDeuce(){
+        return "Empate";
+    }
+    public String getAll(){
+        return "Iguales";
+    }
+    
+    public String getAdvantage(){
+        return "Ventaja ";
+    }
+    public String getWin(){
+        return " Gana!";
+    }
+    
+}
